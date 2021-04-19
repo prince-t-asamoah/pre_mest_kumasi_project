e72814cd073722305ecaa9c184afc4d4924a3a31
PRINCE TWUMASI ASAMOAH <princetwumasam@gmail.com>
2021-04-19 21:12:30 +0000

    Login and signup component updated with useState

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,4 +1,5 @@
 import { useHistory } from 'react-router-dom';
+import { useState } from 'react';
 
 import Inputs from "../../components/Inputs/inputs";
 import Buttons from "../../components/Buttons/buttons";
@@ -7,11 +8,26 @@ import "../../css/w3.css";
 import "./login.css";
 
 function Login() {
-
     const appHistory = useHistory();
-
+    const [userName, setUserName] = useState("");
+    const [userPassword, setUserPassword] = useState("");
+    
     function toSignUp() {
         appHistory.push("/signup");
+    };
+
+    function updateUserName(event) {
+        setUserName(event.target.value);
+        console.log(userName);
+    };
+
+    function updateUserPassword(event) {
+        setUserPassword(event.target.value);
+        console.log(userPassword);
+    };
+
+    function submitButton(event) {
+        event.preventDefault();
     };
 
     return(
@@ -24,16 +40,16 @@ function Login() {
                 <form> 
                     <div className="w3-margin-bottom">
                         <label>Username</label>
-                        <Inputs type="text" placeholder="Enter Your Username"/>
+                        <Inputs type="text" placeholder="Enter Your Username" onchange={updateUserName}/>
                     </div>
 
                     <div className="w3-margin-bottom">
                         <label>Password</label>
-                        <Inputs type="password" placeholder="Enter Your Password"/>
+                        <Inputs type="password" placeholder="Enter Your Password" onchange={updateUserPassword}/>
                     </div>
 
                     <p><a href="#">Forgot password?</a></p>
-                    <Buttons text="Login"/>
+                    <Buttons text="Login" onclick={submitButton}/>
                     <p>Don't have an account? Sign up <a href="#" onClick={toSignUp}>here</a></p>
                 </form>
             </div>
--- src/pages/signup/signup.js
+++ src/pages/signup/signup.js
@@ -1,4 +1,5 @@
 import { useHistory } from 'react-router-dom';
+import { useState } from 'react';
 
 import Inputs from "../../components/Inputs/inputs";
 import Buttons from "../../components/Buttons/buttons";
@@ -8,11 +9,38 @@ import "../../css/w3.css";
 function Signup() {
 
     const appHistory = useHistory();
+    const [userName, setUserName] = useState("");
+    const [userEmail, setEmail] = useState("");
+    const [userPass, setUserPass] = useState("");
+    const [confirmUserPass, setConfirmUserPass] = useState("");
 
     function toLoginPage() {
         appHistory.push("/login");
     };
 
+    function updateUserName(event) {
+        setUserName(event.target.value);
+        console.log(userName);
+    };
+
+    function updateEmail(event) {
+        setEmail(event.target.value);
+        console.log(userEmail);
+    };
+
+    function updateUserPass(event) {
+        setUserPass(event.target.value);
+        console.log(userPass);
+    };
+
+    function updateConfirmPass(event) {
+        setConfirmUserPass(event.target.value);
+        console.log(confirmUserPass);
+    };
+
+    function submitButton(event) {
+        event.preventDefault();
+    };
     return(
         <div className="w3-container">
             <div className="w3-container">
@@ -22,21 +50,26 @@ function Signup() {
             <div className="w3-container">
                 <form>
                     <div className="w3-margin-bottom">
+                        <label>Username</label>
+                        <Inputs type="text" placeholder="Enter Your Username" onchange={updateUserName}/>
+                    </div>
+
+                    <div className="w3-margin-bottom">
                         <label>Email</label>
-                        <Inputs type="text" placeholder="Enter Your Email"/>
+                        <Inputs type="text" placeholder="Enter Your Last Email" onchange={updateEmail}/>
                     </div>
 
                     <div className="w3-margin-bottom">
                         <label>Password</label>
-                        <Inputs type="password" placeholder="Enter Your Password"/>
+                        <Inputs type="password" placeholder="Enter Your Password" onchange={updateUserPass}/>
                     </div>
 
                     <div className="w3-margin-bottom">
                         <label>Confirm Password</label>
-                        <Inputs type="password" placeholder="Repeat Your Password"/>
+                        <Inputs type="password" placeholder="Repeat Your Password" onchange={updateConfirmPass}/>
                     </div>
 
-                    <Buttons text="Sign Up" />
+                    <Buttons text="Sign Up" onclick={submitButton}/>
                     <p>Have an account? Login <a href="#" onClick={toLoginPage}>here</a></p>
                 </form>
             </div>
